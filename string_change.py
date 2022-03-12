638e550e5b29a7c227da367c80700f46c4ccc510
Rohit Gagare <100681982+rgagare@users.noreply.github.com>
2022-03-12 23:55:45 +0530

    Create string_change.py

---
+++ string_change.py
@@ -0,0 +1,9 @@
+a = "Hello, Rohit..."
+b = " Hello, Rohit.."
+print(a.upper())
+print(a.lower())
+print(b.strip())
+print(a.replace("H", "O"))
+print(a.split("!"))
+print("Original="+a)
+print("original="+b)
